a6dae2fae5d1cc63867a529c0f326164baf4a466
Galdineris <galdineris@gmail.com>
2019-05-24 16:37:04 -0300

    Main charts finished

--- Memory Lane/Controllers/Main/MainViewController.swift
+++ Memory Lane/Controllers/Main/MainViewController.swift
@@ -14,11 +14,9 @@ class RatingsChartViewController: UIViewController, DataModifiedDelegate{
     private var dados:[PersonCard] = []
     
     @IBOutlet weak var ratingsChart: DoughnutChartView!
-    @IBOutlet weak var colorsStackView: UIStackView!
-    @IBOutlet weak var ratingsLabelsStackView: UIStackView!
+    @IBOutlet weak var colorsLabelsStackView: UIStackView!
     @IBOutlet weak var numberOfRatingsStackView: UIStackView!
-    
-    
+    @IBOutlet weak var lblWarning: UILabel!
     
     
     //Internal Methods
@@ -27,11 +25,16 @@ class RatingsChartViewController: UIViewController, DataModifiedDelegate{
         ModelManager.shared().addDelegate(newDelegate: self)
         DataModified()
         setupChart()
+        if dados.isEmpty{
+            lblWarning.text = "Nenhuma avaliação encontrada"
+        }else{
+            lblWarning.text = "\(ratingsChart.chartValues.reduce(0,+)) Avaliações"
+        }
     }
     
     public func DataModified() {
         getData()
-        updateChart()
+        setupChart()
     }
     
     private func getData(){
@@ -39,6 +42,13 @@ class RatingsChartViewController: UIViewController, DataModifiedDelegate{
     }
     
     public func updateChart(){
+        ratingsChart.chartValues = getQtdOfRatings()
+        
+        for i in 0...4{
+            if let lbl = numberOfRatingsStackView.arrangedSubviews[i] as? UILabel{
+                lbl.text = String(ratingsChart.chartValues[i])
+            }
+        }
         
     }
     
@@ -60,23 +70,39 @@ class RatingsChartViewController: UIViewController, DataModifiedDelegate{
                     blue: 109/255,
                     alpha: 0.35),
             //918088 - %47
-            UIColor(red: 145/255,
-                    green: 128/255,
-                    blue: 136/255,
-                    alpha: 0.47),
-            //3E3238 - %47
             UIColor(red: 151/255,
                     green: 151/255,
                     blue: 151/255,
-                    alpha: 0.47)]
+                    alpha: 0.47),
+            //918088 - %100
+            UIColor(red: 151/255,
+                    green: 151/255,
+                    blue: 151/255,
+                    alpha: 1.0)]
+        
         ratingsChart.arcWidth = min(ratingsChart.frame.height, ratingsChart.frame.width)/5
+        
+        for i in 0...4{
+            if let lbl = colorsLabelsStackView.arrangedSubviews[i] as? ChartLabelView{
+                lbl.colour = ratingsChart.chartColors[i]
+            }
+            
+        }
         updateChart()
     }
     
-    private func getRatingsValues() -> [Int]{
-        let ratingsValues:[Int] = []
-        
-        return ratingsValues
+    private func getQtdOfRatings() -> [Int]{
+        var ratingsValues:[Int] = [0,0,0,0,0]
+        for i in dados{
+            if let x = i.ratings{
+                for j in x.array{
+                    if let y = (j as! Rating).value?.intValue{
+                        ratingsValues[y-1] += 1
+                    }
+                }
+            }
+        }
+        return ratingsValues.reversed()
     }
     
 }
